c642c0673450a687a84f1b4113fe7dc69d761f46
Marek Krzyszczyk <marek.krzyszczyk@wp.pl>
2020-03-22 22:49:12 +0100

    main class 3

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,4 +1,5 @@
 import model.Author;
+import model.Customer;
 import service.BookService;
 
 import java.util.Scanner;
@@ -26,17 +27,7 @@ public class Apps {
         System.out.println("10: exit");
 
         int input = 0;
-        System.out.println("input to: " + input);
         while (input != 10) {
-//            System.out.println("2: lent a book");
-//            System.out.println("3: remove a book");
-//            System.out.println("4: findAllBooks");
-//            System.out.println("5: findBookDetails");
-//            System.out.println("6: findBookByTitle");
-//            System.out.println("7: findBooksByAuthor");
-//            System.out.println("8: findBooksByYear");
-//            System.out.println("9: findBookByTitleAndAuthor");
-//            System.out.println("10: exit");
             System.out.println("Input number of action you want to do: ");
             input = scanner.nextInt();
             scanner.nextLine();
@@ -58,6 +49,16 @@ public class Apps {
                     scanner.nextLine();
                     service.addBook(titleOfNewBook, yearOfNewBook, authorOfNewBook, nbOfCopies);
                     break;
+                case 2:
+                    Integer idOfLentBook = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Input name of Customer: ");
+                    String nameOfCustomer = scanner.nextLine();
+                    System.out.println("Input name of Customer: ");
+                    String lastNameOfCustomer = scanner.nextLine();
+                    Customer customer = new Customer(nameOfCustomer, lastNameOfCustomer);
+                    service.lentBook(idOfLentBook, customer);
+                    break;
                 case 3:
                     System.out.println("Input id of book to remove");
                     Integer id = scanner.nextInt();
@@ -66,19 +67,6 @@ public class Apps {
                 case 4:
                     System.out.println("List of all books:");
                     service.findAllBooks();
-//                    int year = scanner.nextInt();
-//                    System.out.println("Input title of book: ");
-//                    String title = scanner.nextLine();
-//                    System.out.println("Input year: ");
-//                    int year = scanner.nextInt();
-//                    System.out.println("Input name of Author: ");
-//                    String name = scanner.nextLine();
-//                    System.out.println("Input lastName of Author: ");
-//                    String lastName = scanner.nextLine();
-//                    Author author = new Author(name, lastName);
-//                    System.out.println("Input number of copies: ");
-//                    Integer nbOfCopies = scanner.nextInt();
-//                    service.addBook(title, year, author, nbOfCopies);
                     break;
                 case 5:
                     System.out.println("Input id of book to display details");
@@ -115,7 +103,5 @@ public class Apps {
                     break;
             }
         }
-
-
     }
 }
